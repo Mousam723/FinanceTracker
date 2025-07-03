841acb5e62b30432577b62e9f6c92cb94518f517
Mousam Das <mousamdas2003@gmail.com>
2025-07-04 01:31:52 +0530

    trash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { AuthProvider } from './context/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-import './index.css'; // ✅ must import this file for Tailwind to work
+import './index.css'; // ✅ must import this file for Tailwind to work 
 
 const App = () => (
   <AuthProvider>
